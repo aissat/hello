b0587eb611833c55cd510b1baee7fb353439ebf0
aissat <mr.aissat@gmail.com>
2015-01-18 22:00:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><? echo "hello in hhvm.dev";  ?></title>
+
+</head>
+<body>
+<h1><?php echo "hello in hhvm.dev"; ?></h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+</body>
+</html>
+
+
+
